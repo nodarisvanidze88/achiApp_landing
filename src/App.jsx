19d1144cb47b3fa89f4f4d7a558e6fe794067ac7
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2025-01-10 00:08:36 +0400

    add logo on landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { Navigation } from "./components/navigation";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
-import { Contact } from "./components/contact";
-import JsonData from "./data/data.json";
-import SmoothScroll from "smooth-scroll";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+// import { Navigation } from './components/navigation';
+// import { Header } from './components/header';
+// import { Features } from './components/features';
+// import { About } from './components/about';
+// import { Services } from './components/services';
+// import { Gallery } from './components/gallery';
+// import { Testimonials } from './components/testimonials';
+// import { Team } from './components/Team';
+// import { Contact } from './components/contact';
+// import JsonData from './data/data.json';
+import SmoothScroll from 'smooth-scroll';
+import { LandingImage } from './components/landingimage';
+import './App.css';
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
+    speed: 1000,
+    speedAsDuration: true,
 });
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+    // const [landingPageData, setLandingPageData] = useState({});
+    // useEffect(() => {
+    //     setLandingPageData(JsonData);
+    // }, []);
 
-  return (
-    <div>
-      <Navigation />
+    return (
+        <div className="image-logo">
+            <LandingImage />
+            {/* <Navigation />
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
@@ -33,9 +35,9 @@ const App = () => {
       <Gallery data={landingPageData.Gallery} />
       <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
+      <Contact data={landingPageData.Contact} /> */}
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/landingimage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const LandingImage = () => {
+    return (
+        <div>
+            <a href="https://app.bsi.ge">
+                <img src="/img/logo.png" className="App-logo" alt="logo" />
+            </a>
+        </div>
+    );
+};
